5b9fae2f699bfe57024071fbb57aadde8782d86d
root <root@ip-10-42-93-138.ec2.internal>
2025-12-27 08:35:00 +0000

    Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 """
-This function for writing an Object to file using a JSON representation
+This function for writing object json representaion to file
 """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """This function for writing an Object to file using a JSON representation"""
+    """This function for writing object json representaion to file"""
 
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)
